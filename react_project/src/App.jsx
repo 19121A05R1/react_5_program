8cb88193c2d1bb234b8ed3d5500bb723e615adcf
vaddiralanagesh0191@gmail.com <vaddiralanagesh0191@gmail.com>
2026-09-10 08:29:02 +0530

    intial commit

---
+++ react_project/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>This is a simple React application.</p>
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
