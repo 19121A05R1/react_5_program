c0707e10de391693b41824a7b487e9127ef40c52
vaddiralanagesh0191@gmail.com <vaddiralanagesh0191@gmail.com>
2026-09-10 08:36:16 +0530

    navbar is added

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,6 +1,7 @@
 function App() {
   return (
     <div>
+      <h1>navigation bar is added</h1>
       <h1>Welcome to My React App</h1>
       <p>This is a simple React application.</p>
       <button>Click Me</button>
